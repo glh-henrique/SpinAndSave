89ddbc3f22a655e2fee0496502c33ea16fd098bf
Guilherme <glh.henrique@gmail.com>
2024-10-21 16:22:00 +0100

    implementing versel analytics

--- src/components/BaseLayout.tsx
+++ src/components/BaseLayout.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import NavBar from './NavBar';
 import { ToastContainer } from 'react-toastify';
+import { Analytics } from "@vercel/analytics/react"
 
 const BaseLayout: React.FC = () => {
   return (
@@ -10,6 +11,7 @@ const BaseLayout: React.FC = () => {
         <NavBar />
         <div className="main-content flex-grow p-4">
           <Outlet />
+          <Analytics />
           <ToastContainer />
         </div>
       </div>
