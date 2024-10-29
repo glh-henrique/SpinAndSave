ea2b36802ac01f6d48ef0f81607751f9ad4a0193
Guilherme <glh.henrique@gmail.com>
2024-10-29 14:21:40 +0000

    styling email verication page

--- src/pages/EmailVerification.tsx
+++ src/pages/EmailVerification.tsx
@@ -1,10 +1,35 @@
-import React, { useEffect } from "react";
-import { useNavigate, useLocation } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { useLocation, Link } from "react-router-dom";
 import { account } from "../appwrite";
+import { Card, CssBaseline, Stack, styled, Typography } from "@mui/material";
+import AppTheme from "../theme/appTheme";
+
+const MuiCard = styled(Card)(({ theme }) => ({
+  display: 'flex',
+  flexDirection: 'column',
+  alignSelf: 'center',
+  width: '100%',
+  padding: theme.spacing(4),
+  gap: theme.spacing(2),
+  margin: 'auto',
+  [theme.breakpoints.up('sm')]: { maxWidth: '450px' },
+  boxShadow: 'hsla(220, 30%, 5%, 0.05) 0px 5px 15px 0px, hsla(220, 25%, 10%, 0.05) 0px 15px 35px -5px',
+  ...theme.applyStyles('dark', {
+    boxShadow: 'hsla(220, 30%, 5%, 0.5) 0px 5px 15px 0px, hsla(220, 25%, 10%, 0.08) 0px 15px 35px -5px',
+  }),
+}));
+
+const SignInContainer = styled(Stack)(({ theme }) => ({
+  height: 'calc((1 - var(--template-frame-height, 0)) * 100dvh)',
+  minHeight: '100%',
+  padding: theme.spacing(2),
+  [theme.breakpoints.up('sm')]: { padding: theme.spacing(4) },
+}));
 
 const Verification: React.FC = () => {
-  const navigate = useNavigate();
   const location = useLocation();
+  const [message, setMessage] = useState<string>("Verific")
+
 
   useEffect(() => {
     const verifyUser = async () => {
@@ -15,23 +40,39 @@ const Verification: React.FC = () => {
 
         if (userId && secret) {
           await account.updateVerification(userId, secret);
-          alert("E-mail verificado com sucesso! Você já pode fazer login.");
-          navigate("/");
+          setMessage("E-mail verificado com sucesso! Você já pode fazer login.");
         } else {
-          alert("Link de verificação inválido.");
-          navigate("/");
+          setMessage("Link de verificação inválido.");
         }
       } catch (error) {
         console.error("Erro na verificação de e-mail:", error);
-        alert("A verificação falhou. Por favor, tente novamente.");
-        navigate("/");
+        setMessage("A verificação falhou. Por favor, tente novamente.");
       }
     };
 
     verifyUser();
   }, [location]);
 
-  return <div>Verificando seu e-mail...</div>;
+  return (
+    <>
+      <AppTheme>
+        <CssBaseline enableColorScheme />
+        <SignInContainer direction="column" justifyContent="space-between">
+          <MuiCard variant="outlined">
+            <Typography component="h2" variant="h4" sx={{ fontSize: 'clamp(2rem, 10vw, 2.15rem)' }}>
+              Verificação de e-mail
+            </Typography>
+
+            <Typography component="p" sx={{ marginBottom: '24px' }}>
+              {message}
+            </Typography>
+
+            <Link to='/'>Voltar para Inicio</Link>
+          </MuiCard>
+        </SignInContainer>
+      </AppTheme>
+    </>
+  );
 };
 
 export default Verification;
